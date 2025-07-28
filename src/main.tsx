6f272d8d216c8a75f4d259fad9bbf9384ccc6776
Tony Amende <tony.amende@gmail.com>
2025-07-28 22:53:58 +0200

    update with starte template

---
+++ src/main.tsx
@@ -0,0 +1,55 @@
+import { IxApplicationContext } from "@siemens/ix-react";
+import "@siemens/ix/dist/siemens-ix/siemens-ix.css";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createHashRouter, RouterProvider } from "react-router-dom";
+import App from "./App.tsx";
+import "./i18n";
+import "./index.css";
+import DevicesPage from "./pages/devices/index.tsx";
+import OverviewPage from "./pages/overview/index.tsx";
+
+function optionalTheme() {
+  if (import.meta.env.VITE_THEME) {
+    const css = `${import.meta.env.BASE_URL}theme/dist/css/brand-theme.css`;
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = css;
+    document.head.appendChild(link);
+
+    const loader = `${import.meta.env.BASE_URL}theme/dist/index.js`;
+    const script = document.createElement("script");
+    script.src = loader;
+    script.type = "module";
+    document.head.appendChild(script);
+
+    document.body.classList.add("theme-brand-dark");
+  }
+}
+
+optionalTheme();
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <OverviewPage />,
+      },
+      {
+        path: "/devices",
+        element: <DevicesPage />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <IxApplicationContext>
+      <RouterProvider router={router}></RouterProvider>
+    </IxApplicationContext>
+  </StrictMode>,
+);
---
+++ src/pages/user-settings/index.tsx
@@ -0,0 +1,125 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Siemens AG
+ *
+ * SPDX-License-Identifier: MIT
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+import { IxRadio, IxRadioGroup, IxTypography } from "@siemens/ix-react";
+import brand from "./brand.png";
+import classic from "./classic.png";
+import styles from "./styles.module.css";
+import clsx from "clsx";
+import { useEffect, useState } from "react";
+import { themeSwitcher } from "@siemens/ix";
+import { useTranslation } from "react-i18next";
+import useShowDemoMessage from "@/hooks/demoMessage";
+
+function ThemeButton(props: {
+  name: string;
+  theme: string;
+  active?: boolean;
+  image: string;
+  onClick?: () => void;
+}) {
+  return (
+    <div
+      className={styles.ThemeButton}
+      onClick={(event) => {
+        event.preventDefault();
+        event.stopPropagation();
+
+        if (props.onClick) {
+          props.onClick();
+        }
+      }}
+    >
+      <div
+        className={clsx(styles.ThemeImagePreview, {
+          [styles.Active]: props.active,
+        })}
+      >
+        <img draggable={false} src={props.image} alt="Siemens brand theme" />
+      </div>
+      <div>
+        <IxRadio
+          id={props.name}
+          checked={props.active}
+          onCheckedChange={(event) => {
+            event.preventDefault();
+            console.log("ThemeButton onCheckedChange", props.theme);
+          }}
+          label={props.name}
+        ></IxRadio>
+      </div>
+    </div>
+  );
+}
+
+export default function UserSettings() {
+  const showDemoMessage = useShowDemoMessage();
+  const { t, i18n } = useTranslation();
+  const [currentTheme, setCurrentTheme] = useState<string>(
+    import.meta.env.VITE_THEME ? "brand" : "classic",
+  );
+
+  useEffect(() => {
+    const currentVariant = themeSwitcher.getCurrentTheme();
+    const isDark = currentVariant.endsWith(themeSwitcher.suffixDark);
+    themeSwitcher.setTheme(`theme-${currentTheme}-${isDark ? "dark" : "light"}`);
+  }, [currentTheme]);
+
+  function changeTheme(theme: string) {
+    if (import.meta.env.VITE_THEME === undefined) {
+      showDemoMessage();
+      return;
+    }
+
+    setCurrentTheme(theme);
+  }
+
+  return (
+    <div className={styles.UserSettings}>
+      <IxTypography format="h4">Theme</IxTypography>
+      <section className={styles.ThemeSelection}>
+        <ThemeButton
+          name="Siemens Brand"
+          image={brand}
+          active={currentTheme === "brand"}
+          theme="brand"
+          onClick={() => changeTheme("brand")}
+        />
+        <ThemeButton
+          name="Classic"
+          image={classic}
+          active={currentTheme === "classic"}
+          theme="classic"
+          onClick={() => changeTheme("classic")}
+        />
+      </section>
+      <section>
+        <IxTypography className={styles.HeadlineLanguage} format="h4">
+          {t("language.title")}
+        </IxTypography>
+        <section className={styles.LanguageSelection}>
+          <IxRadioGroup>
+            <IxRadio
+              id="l_en"
+              checked={i18n.language === "en"}
+              onCheckedChange={() => i18n.changeLanguage("en")}
+              label={t("language.en")}
+            ></IxRadio>
+            <IxRadio
+              id="l_de"
+              checked={i18n.language === "de"}
+              onCheckedChange={() => i18n.changeLanguage("de")}
+              label={t("language.de")}
+            ></IxRadio>
+          </IxRadioGroup>
+        </section>
+      </section>
+    </div>
+  );
+}

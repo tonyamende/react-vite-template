0bc9bad263356050e4b64d8693d6ff45927cf399
Tony Amende <tony.amende@gmail.com>
2025-07-28 20:33:09 +0200

    update siemens ix

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './styles/global.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { IxApplicationContext } from '@siemens/ix-react';
+import App from './App';
+
+const root = createRoot(document.getElementById('root') as HTMLElement);
+
+root.render(
+  <React.StrictMode>
+    <IxApplicationContext>
+      <App />
+    </IxApplicationContext>
+  </React.StrictMode>
+);

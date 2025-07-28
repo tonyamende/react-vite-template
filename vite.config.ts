6450831bb9a5710151c5cbc9f3671a2d64fa1d1b
Tony Amende <tony.amende@gmail.com>
2025-07-28 23:23:05 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import fs from "fs-extra";
 
-// https://vite.dev/config/
+const base = process.env.REACT_BASE || "/";
+
+function checkForAdditionalTheme() {
+  try {
+    const themePackage = import.meta.resolve("@siemens-ix/corporate-theme");
+    const theme = path.join(themePackage.replace("file://", ""), "..", "..");
+
+    fs.copySync(theme, path.join(__dirname, "public", "theme"), {
+      filter: (src) => {
+        return !src.includes("corporate-theme-alternative/node_modules");
+      },
+    });
+    console.log("Load additional theme");
+  } catch (e) {
+    console.log("No additional theme found", e);
+  }
+}
+
+checkForAdditionalTheme();
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  base,
+  base:"react-vite-template",
   plugins: [react()],
-  base: '/react-vite-template/', // 👈 Must match your repo name
-})
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: path.resolve(__dirname, "src"),
+      },
+    ],
+  },
+  test: {
+    setupFiles: "./src/setupTests.ts",
+    browser: {
+      enabled: true,
+      provider: "playwright",
+      instances: [{ browser: "chromium" }],
+    },
+  },
+});
